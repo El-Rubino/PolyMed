9b7f935f5f3776e2fd7d372f59460c3949d9c667
Ruben <ruben.hategan94@gmail.com>
2021-07-06 21:08:21 +0300

    add header and body

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderBarComponent } from "./header/header-bar/header-bar.component";
+import { HeaderComponent } from "./header/header.component";
+import { BodyComponent } from "./body/body.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'navBar', component:HeaderBarComponent},
+  { path: 'header_main', component:HeaderComponent},
+  { path: 'body', component:BodyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderBarComponent } from './header/header-bar/header-bar.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderBarComponent,
+    HeaderComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
